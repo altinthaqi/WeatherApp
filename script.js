4c7703f3da973ce7a0e7c36373c40bb4b590b22d
Altin Thaqi <ltin.thaqi.520@gmail.com>
2021-03-09 16:45:31 +0100

    Finished functionality, implemented API, fetch data and show data

--- script.js
+++ script.js
@@ -21,5 +21,46 @@ function getResults(query) {
 }
 
 function displayResults(weather) {
-  console.log(weather);
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector(".current .temp");
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>C</span>`;
+
+  let weather_element = document.querySelector(".current .weather");
+  weather_element.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${Math.round(weather.main.temp_min)}C / ${Math.round(
+    weather.main.temp_max
+  )}C`;
+}
+
+function dateBuilder(d) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const days = ["Sun", "Mon", "Tues", "Wed", "thurs", "Fri", "Sat"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year} `;
 }
